392d9c07d3a41803f6e63bbdcba1b37c23580de5
toby676 <tjcmui@gmail.com>
2016-07-12 14:55:22 +0100

    add restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+end
